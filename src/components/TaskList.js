c14aff2894b00c4df4f45637ce6c54b6c4be9fb1
Eric Song <ericksong91@gmail.com>
2022-10-17 03:05:46 -0400

    commit tree 46d1b2282824445245fcee4e6bd15c36e8cac3af

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, handleDelete }) {
+
+  function mapTasks (arr) {
+   return arr.map((task)=>{
+      return <Task key={task.text} text={task.text} category={task.category} handleDelete={handleDelete}/>
+    })
+  }
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {mapTasks(tasks)}
     </div>
   );
 }
